6c802dc888b537939c4a2f1b61ffbb701726826a
kristentlbt <kristentlbt23@gmail.com>
2021-11-10 21:22:52 -0500

    bootstrap plantae case study

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(".card1, .card2, .card3").hover(function() {
+    $(this).css("box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2)")
+  });
+
+  $(".homeButton, .resumeButton, .projectsButton, .aboutMeButton").hover(function() {
+    $(this).css("text-decoration: underline text-decoration-color: #e75480");
+  });
